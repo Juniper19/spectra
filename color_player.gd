255086449fe94eff310b4e27ce417ae76fccc27a
Rylee Johnston <junipear19@gmail.com>
2025-10-01 14:48:24 -0600

    color swapping to platforms works!! BASE FUNCTIONAL STATE!

--- color_player.gd
+++ color_player.gd
@@ -1,10 +1,11 @@
 extends CharacterBody2D
+
 # Movement settings
 @export var speed: float = 200.0
 @export var jump_force: float = 400.0
 @export var gravity: float = 1000.0
 
-#Color settings
+# Color settings
 @export var colors: Array[Color] = [Color.RED, Color.GREEN, Color.BLUE]
 var current_color_index: int = 0
 
@@ -12,9 +13,10 @@ var current_color_index: int = 0
 
 func _ready() -> void:
 	sprite.modulate = colors[current_color_index]
+	update_collision_masks()
 
 func _physics_process(delta: float) -> void:
-	# Apply gravity
+	# gravity yuh
 	if not is_on_floor():
 		velocity.y += gravity * delta
 	else:
@@ -38,3 +40,19 @@ func _physics_process(delta: float) -> void:
 func cycle_color() -> void:
 	current_color_index = (current_color_index + 1) % colors.size()
 	sprite.modulate = colors[current_color_index]
+	update_collision_masks()
+
+func update_collision_masks() -> void:
+	set_collision_mask_value(1, true) # white
+	set_collision_mask_value(2, false) # red
+	set_collision_mask_value(3, false) # blue
+	set_collision_mask_value(4, false) # green
+
+	# Enable the current color
+	match current_color_index:
+		0: # red
+			set_collision_mask_value(2, true)
+		1: # green
+			set_collision_mask_value(4, true)
+		2: # blue
+			set_collision_mask_value(3, true)
