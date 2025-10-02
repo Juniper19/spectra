37c9fd2fd69aab25647540c2162d6423d2ad3484
Rylee Johnston <junipear19@gmail.com>
2025-10-02 12:54:36 -0600

    death logic works

--- color_player.gd
+++ color_player.gd
@@ -8,12 +8,16 @@ extends CharacterBody2D
 # Color settings
 @export var colors: Array[Color] = [Color.RED, Color.GREEN, Color.BLUE]
 var current_color_index: int = 0
+var spawn_position: Vector2
+
 
 @onready var sprite: AnimatedSprite2D = $Block
 
 func _ready() -> void:
 	sprite.modulate = colors[current_color_index]
 	update_collision_masks()
+	
+	spawn_position = global_position
 
 func _physics_process(delta: float) -> void:
 	# gravity yuh
@@ -36,6 +40,9 @@ func _physics_process(delta: float) -> void:
 	# Color cycling
 	if Input.is_action_just_pressed("ui_accept"): # space
 		cycle_color()
+		
+	# check for deathpit overlap
+	check_deathpit()
 
 func cycle_color() -> void:
 	current_color_index = (current_color_index + 1) % colors.size()
@@ -47,6 +54,8 @@ func update_collision_masks() -> void:
 	set_collision_mask_value(2, false) # red
 	set_collision_mask_value(3, false) # blue
 	set_collision_mask_value(4, false) # green
+	
+	set_collision_mask_value(16, true) # deathpit (always active)
 
 	# Enable the current color
 	match current_color_index:
@@ -56,3 +65,15 @@ func update_collision_masks() -> void:
 			set_collision_mask_value(4, true)
 		2: # blue
 			set_collision_mask_value(3, true)
+
+#------------------------Death logic-------------------------------------------
+func check_deathpit() -> void:
+	for i in range(get_slide_collision_count()):
+		var collision = get_slide_collision(i)
+		var collider = collision.get_collider()
+		if collider is TileMapLayer and collider.name == "DeathPitLayer":
+			respawn()
+			
+func respawn() -> void:
+	global_position = spawn_position
+	velocity = Vector2.ZERO
